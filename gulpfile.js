f166c03d9d0d5a01f3339f2a4c09cfb2e700ed6e
LeoMSSilva <l.eom1994@hotmail.com>
2022-03-13 15:22:15 -0300

    Refactor: changed gulp architecture and gulp settings

--- gulpfile.js
+++ gulpfile.js
@@ -1,133 +1,82 @@
-"use strict";
-
-// Load plugins
-const autoprefixer = require("gulp-autoprefixer");
-const browsersync = require("browser-sync").create();
-const cleanCSS = require("gulp-clean-css");
-const del = require("del");
-const gulp = require("gulp");
-const header = require("gulp-header");
-const merge = require("merge-stream");
-const plumber = require("gulp-plumber");
-const rename = require("gulp-rename");
+const browsersync = require('browser-sync').create();
+const del = require('del');
+const gulp = require('gulp');
+const merge = require('merge-stream');
+const plumber = require('gulp-plumber');
 const sass = require('gulp-sass')(require('sass'));
-const uglify = require("gulp-uglify");
+const autoprefixer = require('gulp-autoprefixer');
+const rename = require('gulp-rename');
+const cleanCss = require('gulp-clean-css');
 
-// Load package.json for banner
-const pkg = require('./package.json');
-
-// Set the banner content
-const banner = ['/*!\n',
-  ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
-  ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
-  ' * Licensed under <%= pkg.license %> (https://github.com/BlackrockDigital/<%= pkg.name %>/blob/master/LICENSE)\n',
-  ' */\n',
-  '\n'
-].join('');
+// Defining paths
+var module = './node_modules/';
+const input = './src/';
+const output = './dist/vendor/';
 
 // BrowserSync
-function browserSync(done) {
-  browsersync.init({
-    server: {
-      baseDir: "./"
-    },
-    port: 3000
-  });
-  done();
-}
+const browserSync = (done) => {
+	browsersync.init({
+		server: {
+			baseDir: './',
+		},
+		port: 3000,
+	});
+	done();
+};
 
 // BrowserSync reload
-function browserSyncReload(done) {
-  browsersync.reload();
-  done();
-}
+const browserSyncReload = (done) => {
+	browsersync.reload();
+	done();
+};
 
-// Clean vendor
-function clean() {
-  return del(["./vendor/"]);
-}
+// Clean output
+const clean = () => del([output]);
 
-// Bring third party dependencies from node_modules into vendor directory
-function modules() {
-  // Bootstrap
-  var bootstrap = gulp.src('./node_modules/bootstrap/dist/**/*')
-    .pipe(gulp.dest('./vendor/bootstrap'));
-  // Font Awesome CSS
-  var fontAwesomeCSS = gulp.src('./node_modules/@fortawesome/fontawesome-free/css/**/*')
-    .pipe(gulp.dest('./vendor/fontawesome-free/css'));
-  // Font Awesome Webfonts
-  var fontAwesomeWebfonts = gulp.src('./node_modules/@fortawesome/fontawesome-free/webfonts/**/*')
-    .pipe(gulp.dest('./vendor/fontawesome-free/webfonts'));
-  // jQuery Easing
-  var jqueryEasing = gulp.src('./node_modules/jquery.easing/*.js')
-    .pipe(gulp.dest('./vendor/jquery-easing'));
-  // jQuery
-  var jquery = gulp.src([
-      './node_modules/jquery/dist/*',
-      '!./node_modules/jquery/dist/core.js'
-    ])
-    .pipe(gulp.dest('./vendor/jquery'));
-  return merge(bootstrap, fontAwesomeCSS, fontAwesomeWebfonts, jquery, jqueryEasing);
-}
+// Bring modules from node_modules to output directory
+const modules = () => {
+	// Bootstrap
+	const bootstrap = gulp
+		.src(module + 'bootstrap/dist/**/*')
+		.pipe(gulp.dest(output + 'bootstrap'));
+	// Font Awesome CSS
+	const fonts = gulp
+		.src(module + '@fortawesome/fontawesome-free/css/**/*')
+		.pipe(gulp.dest(output + 'fontawesome-free/css'));
+	// Font Awesome Webfonts
+	const webFonts = gulp
+		.src(module + '@fortawesome/fontawesome-free/webfonts/**/*')
+		.pipe(gulp.dest(output + 'fontawesome-free/webfonts'));
+
+	return merge(bootstrap, fonts, webFonts);
+};
 
 // CSS task
-function css() {
-  return gulp
-    .src("./scss/**/*.scss")
-    .pipe(plumber())
-    .pipe(sass({
-      outputStyle: "expanded",
-      includePaths: "./node_modules",
-    }))
-    .on("error", sass.logError)
-    .pipe(autoprefixer({
-      cascade: false
-    }))
-    .pipe(header(banner, {
-      pkg: pkg
-    }))
-    .pipe(gulp.dest("./css"))
-    .pipe(rename({
-      suffix: ".min"
-    }))
-    .pipe(cleanCSS())
-    .pipe(gulp.dest("./css"))
-    .pipe(browsersync.stream());
-}
-
-// JS task
-function js() {
-  return gulp
-    .src([
-      './js/*.js',
-      '!./js/*.min.js'
-    ])
-    .pipe(uglify())
-    .pipe(header(banner, {
-      pkg: pkg
-    }))
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('./js'))
-    .pipe(browsersync.stream());
-}
+const css = () =>
+	gulp
+		.src(input + 'scss/**/*.scss')
+		.pipe(plumber())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer({ cascade: false }))
+		.pipe(gulp.dest(input + 'css'))
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(cleanCss())
+		.pipe(gulp.dest(input + 'css'))
+		.pipe(browsersync.stream());
 
 // Watch files
-function watchFiles() {
-  gulp.watch("./scss/**/*", css);
-  gulp.watch(["./js/**/*", "!./js/**/*.min.js"], js);
-  gulp.watch("./**/*.html", browserSyncReload);
-}
+const watchFiles = () => {
+	gulp.watch(input + 'scss/**/*', css);
+	gulp.watch('./**/*.html', browserSyncReload);
+};
 
-// Define complex tasks
+// Task sequence defined
 const vendor = gulp.series(clean, modules);
-const build = gulp.series(vendor, gulp.parallel(css, js));
+const build = gulp.series(vendor, css);
 const watch = gulp.series(build, gulp.parallel(watchFiles, browserSync));
 
 // Export tasks
 exports.css = css;
-exports.js = js;
 exports.clean = clean;
 exports.vendor = vendor;
 exports.build = build;
